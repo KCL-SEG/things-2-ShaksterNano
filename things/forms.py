2bc4fe7dc39e605b56b2a1cfffc6c8136d806a01
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2022-10-20 17:12:11 +0100

    Complete exercises

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,11 @@
 """Forms of the project."""
+from django.forms import ModelForm, Textarea, NumberInput, Widget
 
-# Create your forms here.
+from things.models import Thing
+
+
+class ThingForm(ModelForm):
+    class Meta:
+        model: Thing = Thing
+        fields: list[str] = ["name", "description", "quantity"]
+        widgets: dict[str, Widget] = {"description": Textarea(), "quantity": NumberInput()}
